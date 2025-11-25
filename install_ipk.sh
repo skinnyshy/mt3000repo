b0117a1ccd1132637ce8f8e3040b5204095b3163
skinnyshy <259374495@qq.com>
2025-11-25 11:33:37 +0800

    更新脚本

--- install_ipk.sh
+++ install_ipk.sh
@@ -22,12 +22,18 @@ OpClashInstall() {
 	if [ "$(readlink /usr/sbin/iptables)" = "/usr/sbin/xtables-nft-multi" ]; then 
 		# openwrt23之后默认使用nftables使用下面的命令进行安装,默认使用opkg管理器
 		opkg update
-		opkg install bash dnsmasq-full curl ca-bundle ip-full ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base
+		opkg install bash dnsmasq-full curl ca-bundle ip-full ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base || {
+		    echo "错误: 依赖包安装失败"
+		    exit 2
+		}
 		opkg install /tmp/openclash.ipk
 
 	else 
 		opkg update
-		opkg install bash iptables dnsmasq-full curl ca-bundle ipset ip-full iptables-mod-tproxy iptables-mod-extra ruby ruby-yaml kmod-tun kmod-inet-diag unzip luci-compat luci luci-base
+		opkg install bash iptables dnsmasq-full curl ca-bundle ipset ip-full iptables-mod-tproxy iptables-mod-extra ruby ruby-yaml kmod-tun kmod-inet-diag unzip luci-compat luci luci-base || {
+		    echo "错误: 依赖包安装失败"
+		    exit 2
+		}
 		opkg install /tmp/openclash.ipk
 	fi
 }
@@ -38,32 +44,39 @@ GlinjectorIns() {
 	cd /tmp/glinjector || exit 3
 	wget --no-check-certificate  -O glinjector.zip https://gh-proxy.org/https://github.com/skinnyshy/mt3000repo/blob/main/glinjector/glinjector_3.0.5-6_all.zip || curl -k -o glinjector.zip https://gh-proxy.org/https://github.com/skinnyshy/mt3000repo/blob/main/glinjector/glinjector_3.0.5-6_all.zip
 	unzip glinjector.zip || exit 3
-	opkg install ./*.ipk || exit 3
+	opkg install *.ipk 
 }
 
 ArgonInstall() {
-	opkg install luci-compat
-	opkg install luci-lib-ipkg
+	opkg install luci-compat luci-lib-ipkg || {
+		    echo "错误: 依赖包安装失败"
+		    exit 4
+		}
 	mkdir -p /tmp/argon || exit 4
 	cd /tmp/argon || exit 4
-	wget --no-check-certificate -O luci-theme-argon.ipk https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.3.2/luci-theme-argon_2.3.2-r20250207_all.ipk || curl -k -o luci-theme-argon.ipk https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.3.2/luci-theme-argon_2.3.2-r20250207_all.ipk
+	wget --no-check-certificate -O luci-theme-argon.ipk https://gh-proxy.org/https://github.com/skinnyshy/mt3000repo/blob/main/luci-argon-theme/luci-theme-argon_2.3.2-r20250207_all.ipk || curl -k -o luci-theme-argon.ipk https://gh-proxy.org/https://github.com/skinnyshy/mt3000repo/blob/main/luci-argon-theme/luci-theme-argon_2.3.2-r20250207_all.ipk
 	opkg install luci-theme-argon*.ipk
 }
+
+Cleanup() {
+    rm -rf /tmp/openclash /tmp/glinjector /tmp/argon
+}
 main() {
-	if [ $(opkg list | grep -i glinjector | wc -l) = 1 ]; then
+	if [ $(opkg list-installed | grep -i glinjector | wc -l) = 1 ]; then
 		echo "glinjector already installed!!"
 	else
 		GlinjectorIns
 	fi
-	if [ $(opkg list | grep -i openclash | wc -l) = 1 ]; then
+	if [ $(opkg list-installed | grep -i openclash | wc -l) = 1 ]; then
 		echo "openclash already installed!!"
 	else
 		OpClashInstall
 	fi
-	if [ $(opkg list | grep -i luci-theme-argon | wc -l) = 1 ]; then
+	if [ $(opkg list-installed | grep -i luci-theme-argon | wc -l) = 1 ]; then
 		echo "argontheme already installed!!"
 	else
 		ArgonInstall
 	fi
+	Cleanup
 	
 }
